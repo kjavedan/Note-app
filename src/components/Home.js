7ec5e1d7518b1a5d87710a40ce64cff587aa875c
khaled javedan <khaledjavedan76@gmail.com>
2022-08-11 22:12:07 +0430

    working on home page default theme navbar (mobile screen size)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import {FiSearch} from 'react-icons/fi'
+import {BsSun, BsMoon} from 'react-icons/bs'
+import {AiOutlineFileAdd} from 'react-icons/ai'
 
 export default function Home() {
   return (
     <div className='home'>
         <nav className="home-nav">
-            <h2 className="home-nav__title">Notes</h2>
-            <button className="home-nav__btn">Create <span className="icon"></span></button>
+            <h2 className="home-nav__title">notes</h2>
+            <button className="home-nav__btn"><span className="mobile"><AiOutlineFileAdd /></span></button>
             <div className="home-nav__search-container">
                 <input type="text" placeholder='Search for your note'/>
-                <span className="search-icon"></span>
+                <span className="search-icon"><FiSearch /></span>
             </div>
             <div className="home-nav__theme-switcher">
-                <div className="defaule-mode"></div>
-                <div className="dark-mode"></div>
+                <div className="default-mode"><BsSun /></div>
+                <div className="dark-mode active"><BsMoon /></div>
             </div>
         </nav>
         <div className="home-notes">
