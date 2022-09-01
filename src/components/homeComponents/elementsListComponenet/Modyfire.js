0b79c84e2950ffd50aefeeaa9da18bbcc7a0636a
khaled javedan <khaledjavedan76@gmail.com>
2022-09-01 11:37:28 +0430

    styling modyfire component

--- src/components/homeComponents/elementsListComponenet/Modyfire.js
+++ src/components/homeComponents/elementsListComponenet/Modyfire.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export default function Modyfire() {
+export default function Modyfire(props) {
   return (
-    <div>Modyfire</div>
+    <div className={`home-modyfire ${props.modificationMode ? 'open' : ''}`}>
+        <div className="modyfire-btns">
+            <button className="add-to-passwords"></button>
+            <button className="add-to-favorite"></button>
+            <button className="change-theme"></button>
+            <button className="delete"></button>
+        </div>
+        <div className="modyfire-left">
+            <span className="counter">1</span>
+            <button className="close-modyfire"></button>
+        </div>
+    </div>
   )
 }
