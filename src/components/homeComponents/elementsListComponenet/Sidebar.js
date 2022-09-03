c776e13eb99ec7078d547a0f86d794481c056944
khaled javedan <khaledjavedan76@gmail.com>
2022-09-03 12:35:26 +0430

    styling the sidebar

--- src/components/homeComponents/elementsListComponenet/Sidebar.js
+++ src/components/homeComponents/elementsListComponenet/Sidebar.js
@@ -6,43 +6,45 @@ import { TiTick } from "react-icons/ti";
 import { BsFiles } from "react-icons/bs";
 
 
-export default function Sidebar() {
+export default function Sidebar(props) {
   return (
-    <div className="home-sidebar">
-      <div className="sidebar-toggler"></div>
+    <div className={`home-sidebar ${props.darkMode ? 'dark' : ''}`}>
+      <div className={`sidebar-toggler ${props.darkMode ? 'dark' : ''}`}></div>
       <div className="sidebar-menu">
       {/* recent */}
-        <div className="category-container">
+        <div className={`category-container ${props.darkMode ? 'dark' : ''}`}>
           <p className="category-name">recent</p>
           <GiBackwardTime />
         </div>
       {/* favorite */}
-        <div className="category-container">
+        <div className={`category-container ${props.darkMode ? 'dark' : ''}`}>
           <p className="category-name">favorite</p>
           <MdFavorite />
         </div>
+        <div className="line"></div>
       {/* All materials */}
-        <div className="category-container">
+        <div className={`category-container ${props.darkMode ? 'dark' : ''}`}>
           <p className="category-name">all materials</p>
           <BsFiles />
         </div>
       {/* Notes */}
-        <div className="category-container">
+        <div className={`category-container ${props.darkMode ? 'dark' : ''}`}>
           <p className="category-name">notes</p>
           <MdOutlineNoteAlt />
         </div>
       {/* Todos */}
-        <div className="category-container">
+        <div className={`category-container ${props.darkMode ? 'dark' : ''}`}>
           <p className="category-name">todos</p>
           <TiTick />
         </div>
       {/* passwords */}
-        <div className="category-container">
+        <div className={`category-container ${props.darkMode ? 'dark' : ''}`}>
           <p className="category-name">passwords</p>
           <MdPassword />
         </div>
+        <div className="line"></div>
       {/* deleted */}
-        <div className="category-container">
+        <div className={`category-container ${props.darkMode ? 'dark' : ''}`}>
           <p className="category-name">deleted</p>
           <FiTrash />
         </div>
