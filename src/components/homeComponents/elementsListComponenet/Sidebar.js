4ef04da452630a1757506b92843375d4bcbfb707
khaled javedan <khaledjavedan76@gmail.com>
2022-09-03 11:42:04 +0430

    working on the sidebar structure

--- src/components/homeComponents/elementsListComponenet/Sidebar.js
+++ src/components/homeComponents/elementsListComponenet/Sidebar.js
@@ -1,9 +1,52 @@
 import React from "react";
+import { GiBackwardTime } from 'react-icons/gi'
+import { MdPassword, MdFavorite, MdOutlineNoteAlt } from "react-icons/md";
+import { FiTrash } from "react-icons/fi";
+import { TiTick } from "react-icons/ti";
+import { BsFiles } from "react-icons/bs";
+
 
 export default function Sidebar() {
   return (
     <div className="home-sidebar">
-      <div className="home-sidebar__menu"></div>
+      <div className="sidebar-toggler"></div>
+      <div className="sidebar-menu">
+      {/* recent */}
+        <div className="category-container">
+          <p className="category-name">recent</p>
+          <GiBackwardTime />
+        </div>
+      {/* favorite */}
+        <div className="category-container">
+          <p className="category-name">favorite</p>
+          <MdFavorite />
+        </div>
+      {/* All materials */}
+        <div className="category-container">
+          <p className="category-name">all materials</p>
+          <BsFiles />
+        </div>
+      {/* Notes */}
+        <div className="category-container">
+          <p className="category-name">notes</p>
+          <MdOutlineNoteAlt />
+        </div>
+      {/* Todos */}
+        <div className="category-container">
+          <p className="category-name">todos</p>
+          <TiTick />
+        </div>
+      {/* passwords */}
+        <div className="category-container">
+          <p className="category-name">passwords</p>
+          <MdPassword />
+        </div>
+      {/* deleted */}
+        <div className="category-container">
+          <p className="category-name">deleted</p>
+          <FiTrash />
+        </div>
+      </div>
     </div>
   );
 }
